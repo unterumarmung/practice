adc3e3c4134aadc6c157da187ef3bfa26bb8e706
unterumarmung <39276703+unterumarmung@users.noreply.github.com>
2018-07-10 17:56:21 +0300

    Delete Number_of_1_Bits_191.c

--- LeetCode/Number_of_1_Bits_191.c
+++
@@ -1,9 +0,0 @@
-//LeetCode link: 
-int hammingWeight(uint32_t number) {
-    int bits_number = 0;
-    while(number){
-        bits_number += number % 2;
-        number >>= 1;
-    }
-    return bits_number;
-}

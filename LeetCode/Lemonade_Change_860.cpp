f60bf015d05f740136fac0c37aad1dfe02556928
unterumarmung <39276703+unterumarmung@users.noreply.github.com>
2018-07-13 13:17:59 +0300

    Update Lemonade_Change_860.cpp

--- LeetCode/Lemonade_Change_860.cpp
+++ LeetCode/Lemonade_Change_860.cpp
@@ -7,21 +7,19 @@ public:
         unsigned short int five = 0;
         unsigned short int ten = 0;
         
-        for(unsigned short int i = 0; i < bills.size() && success; ++i){
-            if(bills[i] == 5) { five += 1; }
+        for (unsigned short int i = 0; i < bills.size() && success; ++i) {
+            if (bills[i] == 5) { five += 1; }
             
-            else if(bills[i] == 10){
-                if(five == 0) success = false;
+            else if (bills[i] == 10) {
+                if (five == 0) success = false;
                 five -= 1;
                 ten += 1;
-            }
-            
-            else{
-                if(ten > 0 && five > 0){
+            } else {
+                if (ten > 0 && five > 0) {
                     five -= 1;
                     ten -= 1;
                 }
-                else if(five >= 3) { five -= 3; } 
+                else if (five >= 3) { five -= 3; } 
                 else { success = false; }
             }
         }

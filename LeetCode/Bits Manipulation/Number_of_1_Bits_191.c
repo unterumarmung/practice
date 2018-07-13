a479fa7d03f8d651153f60a46742eb7fbb815619
unterumarmung <39276703+unterumarmung@users.noreply.github.com>
2018-07-13 13:15:45 +0300

    Style changes

--- LeetCode/Bits Manipulation/Number_of_1_Bits_191.c
+++ LeetCode/Bits Manipulation/Number_of_1_Bits_191.c
@@ -1,8 +1,8 @@
 //LeetCode link: https://leetcode.com/problems/number-of-1-bits/
 int hammingWeight(uint32_t number) {
     int bits_number = 0;
-    while(number){
-        bits_number += number % 2;
+    while (number) {
+        bits_number += number & 1;
         number >>= 1;
     }
     return bits_number;

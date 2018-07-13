d5ea5142b1a1495dba27803a34640624b24f4845
unterumarmung <39276703+unterumarmung@users.noreply.github.com>
2018-07-13 13:22:25 +0300

    Update Remove_Dulicates_from_Sorted_Array_26.c

--- LeetCode/Remove_Dulicates_from_Sorted_Array_26.c
+++ LeetCode/Remove_Dulicates_from_Sorted_Array_26.c
@@ -2,10 +2,12 @@
 
 int removeDuplicates(int* nums, int nums_size) {
     int nums_count = 0; //amount of unique elements
-    for(int i = 0; i < nums_size; ++i){
+    for (int i = 0; i < nums_size; ++i) {
         nums[nums_count] = nums[i];
-        while(nums[nums_count] == nums[i]) 
+        
+        while (nums[nums_count] == nums[i]) 
             ++i;
+        
         nums_count += 1;
         i -= 1;
     }

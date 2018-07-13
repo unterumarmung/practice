d0701e2f233b95287e5691b3231ee3464c853b5a
unterumarmung <39276703+unterumarmung@users.noreply.github.com>
2018-07-13 13:27:20 +0300

    Update Valid_Palindrome_125.java

--- LeetCode/String/Valid_Palindrome_125.java
+++ LeetCode/String/Valid_Palindrome_125.java
@@ -3,12 +3,12 @@ class Solution {
     private boolean isThereAnyAlphanumeric(String str)
     {
         int size = str.length();
-        for (int i = 0; i < size; ++i){
+        for (int i = 0; i < size; ++i) {
             if (isalnum(str.charAt(i))) return true;
         }
         return false;
     }
-    private boolean isalnum(char c){
+    private boolean isalnum(char c) {
         return Character.isLetter(c) || Character.isDigit(c);
     }
     public boolean isPalindrome(String str) {
@@ -22,15 +22,15 @@ class Solution {
 
         while (begin < strLength && end >= 0 && good)
         {
-            while ( begin < strLength && !isalnum(str.charAt(begin)) ) { begin += 1; }
-            while ( end >= 0 && !isalnum(str.charAt(end)) ) { end -= 1; }
+            while (begin < strLength && !isalnum(str.charAt(begin))) { begin += 1; }
+            while (end >= 0 && !isalnum(str.charAt(end))) { end -= 1; }
             
-            if(begin >= strLength || end < 0);
+            if (begin >= strLength || end < 0);
             else {
                 char beginChar = Character.toLowerCase(str.charAt(begin));
                 char endChar = Character.toLowerCase(str.charAt(end));
 
-                if ( beginChar != endChar ) { good = false; }
+                if (beginChar != endChar) { good = false; }
 
                 begin += 1;
                 end -= 1;

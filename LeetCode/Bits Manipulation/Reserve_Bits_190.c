0e8281002158f68255dd2eb2ea11d77273306ad8
unterumarmung <39276703+unterumarmung@users.noreply.github.com>
2018-07-13 13:16:51 +0300

    Style changes

--- LeetCode/Bits Manipulation/Reserve_Bits_190.c
+++ LeetCode/Bits Manipulation/Reserve_Bits_190.c
@@ -2,9 +2,8 @@
 uint32_t reverseBits(uint32_t number) {
     uint32_t result = 1;
 
-    for(int i = 0; i < 32; ++i)
-    {
-        uint_least8_t bit = number % 2; //contains i-th bit of the number
+    for (int i = 0; i < 32; ++i) {
+        uint_least8_t bit = number & 1; //contains i-th bit of the number
         number >>= 1; //move the number to rigth for one bit
         result <<= 1; //move the result to left for one bit
         result += bit; 

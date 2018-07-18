e683411106bfa19ec0bed4f151101e8bde41a2cd
unterumarmung <39276703+unterumarmung@users.noreply.github.com>
2018-07-18 22:52:04 +0300

    Add files via upload

---
+++ LeetCode/Array/Remove_Duplicates_from_Sorted_Array_26.c
@@ -0,0 +1,18 @@
+//LeetCode Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+/*
+Complexity analysis:
+* Time: O(nums_size)
+* Space: O(1)
+*/
+int removeDuplicates(int* nums, int nums_size) {
+    int nums_count = 0; //amount of unique elements
+    for(int i = 0; i < nums_size; ++i){
+        nums[nums_count] = nums[i];
+        while(nums[nums_count] == nums[i]) 
+            ++i;
+        nums_count += 1;
+        i -= 1;
+    }
+    return nums_count;
+}

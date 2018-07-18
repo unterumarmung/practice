cd3ef2af59b7f5cb980d12335767bdc8041acc5c
unterumarmung <39276703+unterumarmung@users.noreply.github.com>
2018-07-18 23:10:49 +0300

    Add files via upload

---
+++ LeetCode/Array/Search_Insert_Partition_35.py
@@ -0,0 +1,9 @@
+#LeetCode Link: https://leetcode.com/problems/search-insert-position/
+
+class Solution:
+    def searchInsert(self, nums, target):
+        for index, num in enumerate(nums):
+            if num >= target:
+                return index
+        
+        return(len(nums)) 
